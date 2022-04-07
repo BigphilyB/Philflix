df9bccb2f2408f27f6f151c3a69b7ec2299b2389
Philippe <philippe.bloemen@gmail.com>
2022-04-07 23:43:08 +0200

    Tutorial gedaan
    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

---
+++ src/Tmdb.js
@@ -0,0 +1,110 @@
+const API_KEY = 'b07d99cb0e7cbba9c64d7c9c6f826c78';
+const API_BASE = 'https://api.themoviedb.org/3';
+
+/*
+    - Originais da Netflix
+    - Recomendados (Trending)
+    - Em alta (Top rated)
+    - Ação
+    - Comédia
+    - Terror
+    - Romance
+    - Documentários
+*/
+
+const basicFetch = async (endpoint) => {
+    const req = await fetch(`${API_BASE}${endpoint}`);
+    const json = await req.json();
+    return json;
+}
+
+export default {
+    getHomeList: async () => {
+        return [
+            {
+                slug: 'originals',
+                title: 'Originais do Netflix',
+                type: 'tv',
+                items: await basicFetch(`/discover/tv?with_network=213&language=en-BR&api_key=${API_KEY}`)
+            },
+            {
+                slug: 'trending',
+                title: 'recommended for you',
+                type: 'tv',
+                items: await basicFetch(`/trending/all/week?language=en-BR&api_key=${API_KEY}`)
+            }, 
+            {
+                slug: 'toprated',
+                title: 'Populair',
+                type: 'movie',
+                items: await basicFetch(`/movie/top_rated?language=en-BR&api_key=${API_KEY}`)
+            },
+            {
+                slug: 'action',
+                title: 'action',
+                type: 'movie',
+                items: await basicFetch(`/discover/movie?with_genres=28&language=en-BR&api_key=${API_KEY}`)
+            },
+            {
+                slug: 'comedy',
+                title: 'comedy',
+                type: 'movie',
+                items: await basicFetch(`/discover/movie?with_genres=35&language=en-BR&api_key=${API_KEY}`)
+            },
+            {
+                slug: 'horror',
+                title: 'horror',
+                type: 'movie',
+                items: await basicFetch(`/discover/movie?with_genres=27&language=en-BR&api_key=${API_KEY}`)
+            },
+            {
+                slug: 'romance',
+                title: 'Romance',
+                type: 'movie',
+                items: await basicFetch(`/discover/movie?with_genres=10749&language=en-BR&api_key=${API_KEY}`)
+            },
+            {
+                slug: 'documentary',
+                title: 'documentarys',
+                type: 'movie',
+                items: await basicFetch(`/discover/movie?with_genres=99&language=en-BR&api_key=${API_KEY}`)
+            }
+        ]
+    },
+    getMovieInfo: async (movieId, type) => {
+        let info = {};
+
+        if(movieId){
+            switch(type){
+                case 'movie':
+                    info = await basicFetch(`/movie/${movieId}?language=en-BR&api_key=${API_KEY}`);
+                break;
+                case 'tv':
+                    info = await basicFetch(`/tv/${movieId}?language=en-BR&api_key=${API_KEY}`);
+                break;
+                default: 
+                    info = null;
+                break;
+            }
+        }
+        return info;
+    },
+    getTrailerVideo: async (movieId, type) => {
+        let trailer = {};
+
+        if(movieId){
+            switch(type){
+                case 'movie':
+                    trailer = await basicFetch(`/movie/${movieId}/videos?language=en-BR&api_key=${API_KEY}`);
+                break;
+                case 'tv':
+                    trailer = await basicFetch(`/tv/${movieId}/videos?language=en-BR&api_key=${API_KEY}`);
+                break;
+                default: 
+                    trailer = null;
+                break;
+            }
+        }
+        return trailer;
+    },
+}
---
+++ src/components/Header/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './styles.css';
+
+function Header( { black } ) {
+  return (
+    <header className={black ? 'black' : ''}>
+        <div className="header--logo">
+          <a href="/">
+            <img alt="Netflix" src="https://i.ibb.co/0sqVJqx/c2f2f883f088022d245a594ef1b314ea.png" />
+          </a>  
+
+        </div>
+        <div className="header--user">
+          <a href="/">
+            <img alt="Usuário" src="https://learning.royalbcmuseum.bc.ca/wp-content/uploads/2014/07/netflix-face.jpg" />
+          </a>
+        </div>
+    </header>
+  );
+}
+
+export default Header;
